17e645572898be3b7c44789ed96fc71b0cafea5d
Raymond Trinh <trinh_r@hotmail.com>
2015-12-25 22:27:56 -0500

    Add Reddit246-Intermediate

---
+++ Reddit246-Intermediate/PrefixTreeMatcher.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reddit246_Intermediate
+{
+    class PrefixTreeMatcher
+    {
+        public bool IsTerminal = false;
+        public PrefixTreeMatcher[] Children = new PrefixTreeMatcher[26];
+
+        public IEnumerable<int> Find(IEnumerable<char> s)
+        {
+            PrefixTreeMatcher node = this;
+            int i = 0;
+            foreach(char c in s)
+            {
+                PrefixTreeMatcher next = node.Children[c - 'A'];
+                if (null == next)
+                {
+                    break;
+                }
+                else
+                {
+                    if (next.IsTerminal)
+                    {
+                        yield return (i + 1);
+                    }
+                    node = next;
+                }
+                ++i;
+            }
+        }
+
+        public void Insert(string s)
+        {
+            PrefixTreeMatcher node = this;
+            int i = 0;
+            int n = s.Length;
+            while (i < n)
+            {
+                var next = node.Children[s[i] - 'A'];
+                if (next != null)
+                {
+                    node = next;
+                    ++i;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            // Append new nodes
+            while (i < n)
+            {
+                node = node.Children[s[i] - 'A'] = new PrefixTreeMatcher();
+                ++i;
+            }
+            node.IsTerminal = true;
+        }
+
+        public static PrefixTreeMatcher ConstructFromList(IEnumerable<string> strings)
+        {
+            PrefixTreeMatcher root = new PrefixTreeMatcher();
+            foreach (string s in strings)
+            {
+                root.Insert(s);
+            }
+            return root;
+        }
+    }
+}
---
+++ Reddit246-Intermediate/Program.cs
@@ -0,0 +1,143 @@
+﻿//https://www.reddit.com/r/dailyprogrammer/comments/3xye4g/20151223_challenge_246_intermediate_letter_splits/
+using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Reddit246_Intermediate
+{
+    class LetterFinder
+    {
+        private static string[] ListOfEmptyString = { "" };
+        private static string[] EmptyList = { };
+        private Lazy<IEnumerable<string>>[] sets;
+        private string haystack;
+        public LetterFinder(string h)
+        {
+            haystack = h;
+            sets = new Lazy<IEnumerable<string>>[h.Length + 1];
+            sets[h.Length] = new Lazy<IEnumerable<string>>(() => ListOfEmptyString);
+            for (int i = 0; i < h.Length; ++i)
+            {
+                int index = i; // Need a copy of i, because closures...
+                sets[index] = new Lazy<IEnumerable<string>>(() =>
+                {
+                    char c = haystack[index];
+                    if ('1' == c)
+                    {
+                        int j = index + 1;
+                        var PartOne = sets[j].Value.Select(tail => "A" + tail);
+                        if (j < haystack.Length)
+                        {
+                            string nextHead = ((char)(haystack[j] - '0' + 'J')).ToString();
+                            var PartTwo = sets[j + 1].Value.Select(tail => nextHead + tail);
+                            return PartOne.Concat(PartTwo);
+                        }
+                        else
+                        {
+                            return PartOne;
+                        }
+                    }
+                    else if ('2' == c)
+                    {
+                        int j = index + 1;
+                        var PartOne = sets[j].Value.Select(tail => "B" + tail);
+                        if (j < haystack.Length && '0' <= haystack[j] && haystack[j] <= '6')
+                        {
+                            string nextHead = ((char)(haystack[j] - '0' + 20 - 1 + 'A')).ToString();
+                            var PartTwo = sets[j + 1].Value.Select(tail => nextHead + tail);
+                            return PartOne.Concat(PartTwo);
+                        }
+                        else
+                        {
+                            return PartOne;
+                        }
+                    }
+                    else if ('3' <= c && c <= '9')
+                    {
+                        string head = ((char)(c - '3' + 'C')).ToString();
+                        return sets[index + 1].Value.Select(tail => head + tail);
+                    }
+                    else
+                    {
+                        return EmptyList;
+                    }
+                });
+            }
+        }
+
+        public IEnumerable<string> GetLetters()
+        {
+            return sets[0].Value;
+        }
+    }
+
+    static class Program
+    {
+        static string[] ListOfEmptyString = { "" };
+        static string[] EmptyList = { };
+
+        static IEnumerable<string> Combos(string s)
+        {
+            var finder = new LetterFinder(s);
+            return finder.GetLetters();
+        }
+
+        static private PrefixTreeMatcher Matcher = null;
+
+        static bool ValidWords(string s)
+        {
+            if (string.IsNullOrWhiteSpace(s))
+            {
+                return true;
+            }
+            foreach (var len in Matcher.Find(s))
+            {
+                if (ValidWords(s.Substring(len)))
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        static Func<T, TR> Memoize<T, TR>(Func<T, TR> f)
+        {
+            var memo = new Dictionary<T, TR>();
+            return delegate (T arg)
+            {
+                TR ans = default(TR);
+                if (memo.TryGetValue(arg, out ans))
+                {
+                    return ans;
+                }
+                else
+                {
+                    return memo[arg] = f(arg);
+                }
+            };
+        }
+
+        static void Main(string[] args)
+        {
+            // More initialisation
+            const string test = "81161625815129412519419122516181571811313518";
+
+            // Initialize matcher
+            var lines = System.IO.File.ReadLines("enable1.txt");
+            Matcher = PrefixTreeMatcher.ConstructFromList(lines.Select(s => s.ToUpper()));
+
+            var timer = System.Diagnostics.Stopwatch.StartNew();
+            var letters = Combos(test);
+            var words = letters.Where(ValidWords); // letters for normal, letters.Where(ValidWords) for bonus
+            foreach (var s in words)
+            {
+                Console.WriteLine(s);
+            }
+            timer.Stop();
+            Console.WriteLine("Time: {0}", timer.Elapsed);
+        }
+    }
+}
